b1990275041e3b506d08e5022325ef6527104b4a
indranil-bhuin <indranil.bhuin@nineleaps.com>
2023-03-17 08:01:18 +0530

    changes in loginscreen and signupscreen style

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -72,20 +72,69 @@ const LoginScreen = ({ navigation }) => {
 };
 
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   input: {
+//     width: '80%',
+//     borderWidth: 1,
+//     borderColor: 'gray',
+//     borderRadius: 5,
+//     padding: 10,
+//     marginVertical: 10,
+//   },
+  
+// });
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    width: '80%',
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 5,
-    padding: 10,
-    marginVertical: 10,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F0EC57'
+    },
+    inputContainer: {
+      width: '80%'
+    },
+    input: {
+      backgroundColor: '#fff',
+      paddingHorizontal: 15,
+      paddingVertical: 10,
+      borderRadius: 10,
+      marginTop: 5
+    },
+    buttonContainer: {
+      width: '60%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 40
+    },
+    button: {
+      backgroundColor: '#0B0A07',
+      width: '100%',
+      padding: 15,
+      borderRadius: 10,
+      alignItems: 'center'
+    },
+    buttonText: {
+      color: '#fff',
+      fontWeight: '700',
+      fontSize: 16
+    },
+    buttonOutline: {
+      backgroundColor: '#fff',
+      marginTop: 5,
+      borderColor: '#0B0A07',
+      borderWidth: 2
+    },
+    buttonOutlineText: {
+      color: '#0B0A07',
+      fontWeight: '700',
+      fontSize: 16
+    }
 });
 
 
